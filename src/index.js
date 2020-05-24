92809e5942087afb1da4f23c3be1ff50ffe39140
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-05-24 06:46:02 +0530

    added video play/pause and toggle button func

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./styles.css";
 //Get Elements
 const player = document.querySelector(".player");
-const video = player.querySelector(".viever");
+
+const video = player.querySelector(".viewer");
 
 const progress = player.querySelector(".progress");
 const progressbar = player.querySelector(".progress__filled");
@@ -11,5 +12,23 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelector("[data-skip]");
 const range = player.querySelector(".player__slider");
 
-//Build out functions
-//Add the event listeners
+/** Build out functions*/
+//play video
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+//update toggle button text
+function updateButton() {
+  const toggleIcon = this.paused ? "►" : "❚❚";
+  toggle.textContent = toggleIcon;
+}
+
+/**Add the event listeners */
+//play/pause video
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
+
+//update button upon play/pause
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
