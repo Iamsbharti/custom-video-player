1f98aa8396be98d4f14dfdc4c185eff6ef5903c1
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-05-24 06:33:54 +0530

    added element selectors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+//Get Elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".viever");
+
+const progress = player.querySelector(".progress");
+const progressbar = player.querySelector(".progress__filled");
+
+const toggle = player.querySelector(".toggle");
+
+const skipButtons = player.querySelector("[data-skip]");
+const range = player.querySelector(".player__slider");
+
+//Build out functions
+//Add the event listeners
